0e35a569d50c5ea4fabb87cfe9c047d8bba8a0bd
juliorosa22 <juliorosa01@live.com>
2021-06-08 16:26:10 -0300

    ajustes_grid_alunos

--- grad/modules/pages/p_alunos.js
+++ grad/modules/pages/p_alunos.js
@@ -55,34 +55,37 @@ PALUNOS.prototype = {
 
     buildSelectList: function(){
         var retorno ='';
+        retorno+='<div><p>Ano Turma:  <select name="ano" id="turma_ano_filter" onchange="buildTurmaAnoGrid()">'+this.getDistinctTurmaAno()+'</select></p>';
+        return retorno;
+    },
+
+    getAlunosByTurma: function (turma_ano){
+      var retorno = '';
+      let subSql='';
+      (turma_ano === "all")?subSql="ORDER BY ano DESC,nome DESC":subSql="WHERE ano LIKE\""+turma_ano+"\" ORDER BY nome DESC"; 
+      var result = alasql("SELECT * FROM ? "+subSql,[aluno_grad]);
+      let it = result[Symbol.iterator]();
+      var anItem = it.next();
+      
+      
+      while (!anItem.done) {
+          retorno += '<tr style="text-align: left;">';
+          retorno += '<td style="text-align: left;">' + anItem.value.nome + '&nbsp;</td>';
+          retorno += '<td style="text-align: left;">' + anItem.value.ano + '&nbsp;</td></tr>';
+          anItem = it.next();
+      }
+      
+      return retorno;
+    },
+
+    buildTurmaAnoGrid: function(){
+      let turma_ano = document.getElementById("turma_ano_filter").value;
+      let newBody= '<table border="0" class="arquivos" summary="">'; 
+      newBody += '<thead><tr class="bg"><strong>&nbsp;</strong></td><th style="text-align: left;" width="320"><strong>' + getLabel('nome').toUpperCase() + '</strong></th><th style="text-align: left;" width="90"><strong>' + 'ANO' + '</strong></th></tr></thead>';
+      newBody+='<tbody>'+this.getAlunosByTurma(turma_ano)+'</tbody></table>';
+      document.getElementById("grid_alunos_grad").innerHTML=newBody;
+    },
     
-        retorno+='<div><p>Ano Turma:  <select name="ano" id="turma_ano_filter" onchange="buildTurmaAnoGrid()">'+this.getDistinctTurmaAno()+'</select></p><br>';
-        
-        return retorno;
-    },
-
-
-    listaAlunos: function(rotulo, identificador) {
-
-        var retorno = '';
-
-        var result = alasql("SELECT * FROM ? ORDER BY ano DESC",[aluno_grad]);
-        let it = result[Symbol.iterator]();
-        var anItem = it.next();
-        
-        
-        while (!anItem.done) {
-            retorno += '<tr style="text-align: left;">';
-            retorno += '<td style="text-align: left;">' + anItem.value.nome + '&nbsp;</td>';
-            retorno += '<td style="text-align: left;">' + anItem.value.ano + '&nbsp;</td></tr>';
-            anItem = it.next();
-        }
-        
-        
-
-        return retorno;
-    },
-
     conteudo: function() {
         // calculations...
         var dContent = '<div id="viewlet-above-content-title"></div><h1 class="documentFirstHeading">' + this.contentTitle + '</h1><div id="viewlet-below-content-title"></div><div id="viewlet-above-content-body"></div><div id="content-core"><div id="parent-fieldname-text"><p>' + this.langTxt['par-02'] + '</p><h2>' + this.langTxt['par-03'] + '</h2>';
@@ -98,30 +101,6 @@ PALUNOS.prototype = {
 
 
 
-function getAlunosByTurma(turma_ano){
-  var retorno = '';
-  let subSql='';
-  (turma_ano === "all")?subSql="ORDER BY ano DESC,nome DESC":subSql="WHERE ano LIKE\""+turma_ano+"\" ORDER BY nome DESC"; 
-  var result = alasql("SELECT * FROM ? "+subSql,[aluno_grad]);
-  let it = result[Symbol.iterator]();
-  var anItem = it.next();
   
   
-  while (!anItem.done) {
-      retorno += '<tr style="text-align: left;">';
-      retorno += '<td style="text-align: left;">' + anItem.value.nome + '&nbsp;</td>';
-      retorno += '<td style="text-align: left;">' + anItem.value.ano + '&nbsp;</td></tr>';
-      anItem = it.next();
-  }
   
-  return retorno;
-}
-  
-  
-  function buildTurmaAnoGrid(){
-    let turma_ano = document.getElementById("turma_ano_filter").value;
-    let newBody= '<table border="0" class="arquivos" summary="">'; 
-    newBody += '<thead><tr class="bg"><strong>&nbsp;</strong></td><th style="text-align: left;" width="320"><strong>' + getLabel('nome').toUpperCase() + '</strong></th><th style="text-align: left;" width="90"><strong>' + 'ANO' + '</strong></th></tr></thead>';
-    newBody+='<tbody>'+getAlunosByTurma(turma_ano)+'</tbody></table>';
-    document.getElementById("grid_alunos_grad").innerHTML=newBody;
-  }
